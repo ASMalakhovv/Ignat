cb43f6a75175a298a77cd4aa24a42556f74f23fc
ASMalakhovv <alsmalahovv@mail.ru>
2022-04-20 14:25:08 +0600

    fulfilled

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -10,7 +10,7 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+        setValue(restoreState('editable-span-value', value))
     }
 
     return (
@@ -33,6 +33,7 @@ function HW6() {
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperEditableSpan/>*/}
             <hr/>
+
         </div>
     )
 }
